c7c7187693fae7ecbc122f95265d80857ffdef7f
Ankit Khadka <khadka.ankit2004@gmail.com>
2025-07-14 17:24:20 +0545

    Adding the view def post_list and adding templates and urls for the function

--- Blog_app/admin.py
+++ Blog_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from Blog_app.models import Post
+
 # Register your models here.
 
 admin.site.register(Post)
--- Blog_app/views.py
+++ Blog_app/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from Blog_app.models import Post
 
 # Create your views here.
 
 
+def post_list(request):
+    posts = Post.objects.all()
+    return render(
+        request,
+        "post_list.html",
+        {"posts": posts},
+    )
