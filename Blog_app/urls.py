406a18b44bafc51ec0b2dc187d5fbfc3ccc4aaf9
Ankit Khadka <khadka.ankit2004@gmail.com>
2025-07-16 16:57:22 +0545

    Making the draft website , and adding the edit and publish button in draft

--- Blog_app/urls.py
+++ Blog_app/urls.py
@@ -4,5 +4,6 @@ from Blog_app import  views
 urlpatterns = [
     path("",views.post_list, name="post-list"),
     path('details/<int:pk>/',views.post_details , name="post-detail"),
-    path('draft/',views.post_list , name='draft-list'),
+    path('draft/',views.draft_list , name='draft-list'),
+    path('detail/<int:pk>/',views.draft_detail , name='draft-detail'),
 ]
--- Blog_app/views.py
+++ Blog_app/views.py
@@ -9,7 +9,7 @@ def post_list(request):
     # posts = Post.objects.exclude(published_at = None)
     return render(
         request,
-        "draft_list.html",
+        "post_list.html",
         {"posts": posts},
     )
     
@@ -18,5 +18,22 @@ def post_details(request,pk):
     return render(
         request,
         "post_detail.html",
+        {"post": post},
+    )
+    
+def draft_list(request):
+    posts = Post.objects.filter(published_at__isnull = True) # data vako
+    # posts = Post.objects.exclude(published_at = None)
+    return render(
+        request,
+        "draft_list.html",
+        {"posts": posts},
+    )
+    
+def draft_detail(request , pk):
+    post = Post.objects.get(pk=pk , published_at__isnull = True)
+    return render(
+        request,
+        "draft_detail.html",
         {"post": post},
     )
