b2e2596cb370acf426947fe7227fe16aa08846db
Ankit Khadka <khadka.ankit2004@gmail.com>
2025-07-17 17:57:50 +0545

    Making the add field and creating the form

--- Blog_app/forms.py
+++ Blog_app/forms.py
@@ -1,7 +1,7 @@
 from django import forms
-from Blog_app.models import Post
+from Blog_app.models import Post  # for validation 
 
 class PostForm(forms.ModelForm):
     class Meta:
-        model = Post
+        model = Post  # kun model aanusar
         fields = ["title","content"]
